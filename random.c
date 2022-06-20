099fbb649cc2472272b483eae2e0d8c3aa802751
Eduardo Menon <66765757+menon23@users.noreply.github.com>
2022-06-20 09:33:11 -0300

    Update random.c

--- random.c
+++ random.c
@@ -1,12 +1,17 @@
-
 #include <stdio.h>
 #include "mtwister.h"
 
 int main() {
   MTRand r = seedRand(1234567);
-  int i;
+
+  int i;/*
   for(i=0; i<10; i++) {
     printf("%f\n", genRand(&r));
-  }
+  }*/
+  int nonce = genRand(&r)*1000000;
+  printf("%d\n", nonce);
+  nonce = genRand(&r)*1000000;
+  printf("%d\n", nonce);
+
   return 0;
 }
