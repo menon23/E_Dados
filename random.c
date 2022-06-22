9dacfcffbaaa595b34dffcc2d1677755b18d8bc7
Eduardo Menon <66765757+menon23@users.noreply.github.com>
2022-06-22 20:23:23 -0300

    Update random.c
    Desenvolvida a estrutura do data dentro de cada bloco

--- random.c
+++ random.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
-#include "mtwister.h"
+#include "mtwister.c"
 
 int main() {
-  MTRand r = seedRand(1234567);
+  MTRand semente = seedRand(1234567);
 
-  int i;/*
-  for(i=0; i<10; i++) {
-    printf("%f\n", genRand(&r));
-  }*/
-  int nonce = genRand(&r)*1000000;
-  printf("%d\n", nonce);
-  nonce = genRand(&r)*1000000;
-  printf("%d\n", nonce);
+  unsigned char data[184];
+  unsigned char nTrans;
 
+  nTrans = (1+ genRandLong(&semente)%61)*3;
+  printf("Numero de transacoes: %d \n", nTrans/3);
+
+  //data deve ser inicializado com 0;
+  printf("Data:\n");
+  printf("Origem\tDestino\tBitcoin\n");
+  for(int i = 0; i<184; i++)
+    data[i] = 0;
+
+  for(int l=0; l<nTrans; l=l+3)
+  {
+    data[l] = genRandLong(&semente)%256;//0-255 endereço de origem
+    printf("%02x\t",data[l]);
+    data[l+1] = genRandLong(&semente)%256;//0-255 endereço de destino
+        printf("%02x\t",data[l+1]);
+    data[l+2] = genRandLong(&semente)%51;//0-50 valor em bitcoins
+    printf("%d\t",data[l+2]);
+    printf("\n");
+  }
   return 0;
 }
